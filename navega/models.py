ae713c0c054088d3e7ed45f452fb3bdca3aebc14
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2016-11-05 17:11:50 -0600

    Base de los views y del modelo

--- navega/models.py
+++ navega/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Embarcacion(models.Model):
+    nombre = models.CharField(max_length=120)
+    eslora = models.DecimalField(max_digits=10, decimal_places=2)
+    calado = models.DecimalField(max_digits=10, decimal_places=2)
+    tipo = models.CharField(max_length=30)
+    carga = models.IntegerField()
+    costo = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha = models.DateField(auto_now=False, auto_now_add=True)
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL)
