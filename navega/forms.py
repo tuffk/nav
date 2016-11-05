699b3c6b4dda8948eb945def5cfb4d79c552e1e6
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2016-11-05 17:29:39 -0600

    Uniendo las cosas con el judio

---
+++ navega/forms.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django import forms
+from .models import Embarcacion
+
+class UserForm(forms.ModelForm):
+    email = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ['email', 'password']
--- navega/views.py
+++ navega/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Embarcacion
+from .forms import UserForm
+
+from django.views.generic import View
 
 # Create your views here.
+class UserLoginView(View):
+
+    template = 'navega/login.html'
+    form_class = UserForm
+
+    def post(self, request):
+        form = self.form_class(data={'email': self.request.POST['email'],
+                                     'password': self.request.POST['password']})
+
+    email = self.request.POST['email']
+    password = self.request.POST['password']
+
+    # aquí se hace el query en la base de datos para autentificar al usuario
+    user = authenticate(username=email, password=password)
