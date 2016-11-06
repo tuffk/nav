a031dd19aba0f36c700d13f30af7aa2d09f762b1
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2016-11-05 18:34:06 -0600

    arreglando mi cáncer

--- navega/urls.py
+++ navega/urls.py
@@ -5,9 +5,9 @@ from .views import *
 urlpatterns = [
     url('^login/$', UserLoginView.as_view(), name='login'),
     url('^logout/$', logoutMethod, name='logout'),
-    url('^ceo/$', UserLoginView.as_view(), name='ceo'),
-    url('^cfo/$', UserLoginView.as_view(), name='cfo'),
-    url('^gman/$', UserLoginView.as_view(), name='gman'),
+    url('^ceo/$', ceo, name='ceo'),
+    url('^cfo/$', cfo, name='cfo'),
+    url('^gman/$', gman, name='gman'),
     url('^norm/$', insertarEmbarcacion, name='norm'),
     url('^success/$', success_create, name='success')
 ]
